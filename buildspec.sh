b9c3f7e0f7edfbde6ce37bbafff7018bb553a6a7
Robert Peterson <robert.peterson@sungardas.com>
2018-04-11 17:37:44 -0400

    Update template.yaml

--- buildspec.sh
+++ buildspec.sh
@@ -10,7 +10,7 @@ aws cloudformation package \
 
 
 aws cloudformation deploy --template-file ./samTemplate.yaml \
-  --capabilities CAPABILITY_IAM \
+  --capabilities CAPABILITY_NAMED_IAM \
   --stack-name SungardAS-aws-services-alerts-destination \
   --parameter-overrides CloudWatchLogDestinationArn=$CLOUDWATCHLOG_DESTINATION_ARN \
   CloudWatchLogGroupName=$CLOUDWATCHLOG_GROUP_NAME ProjectImage=$PROJECT_IMAGE
